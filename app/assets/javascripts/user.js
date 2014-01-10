6a1ce19fb7069effe4021ede3d730dffddbce09a
wst07261144 <wst07261144@gmail.com>
2014-01-10 13:07:06 +0800

    添加验证码

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,28 +1,19 @@
-/**
- * Created with JetBrains RubyMine.
- * User: wst
- * Date: 14-1-9
- * Time: 下午4:45
- * To change this template use File | Settings | File Templates.
- */
-document.ready(function(){
+$(document).ready(function(){
     code_is_used();
 })
 
 function code_is_used(){
     var code = document.getElementById('code').value;
-    console.log('code-->' + code);
+    console.log('--------code-->' + code);
     $.ajax({
         url:'/code/used/'+code,
         method:'get',
         success: success_call_back,
         error:error_call_back
     })
-
     function success_call_back(result) {
-        alert('success')
+        result=='ok'? window.location.href = 'http://www.jd.com' : setTimeout('code_is_used()',2000)
     }
-
     function error_call_back(error) {
         alert('error')
     }
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,13 @@ class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:change_to_login,:change_to_root_page]
 
   def two_dimensional_code
-    @code = rand(10).to_s
+    code = rand(10)+rand(100)+rand(1000)
+    @code = code.to_s
     @url = 'http://192.168.1.132:3001/sessions/login/'+@code
   end
 
   def is_used
-    render RestClient.get 'localhost:3001/code/used'+params[:code]
+    render :text =>  RestClient.get('localhost:3001/code/used/'+params[:code])
   end
 
 end
