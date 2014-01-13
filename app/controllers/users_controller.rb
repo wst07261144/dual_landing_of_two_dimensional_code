d393cbbbccb55d8f0579f7af20261f2d01c9030e
wst07261144 <wst07261144@gmail.com>
2014-01-13 16:49:28 +0800

    修改格式

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token, :only => [:change_to_login,:change_to_root_page]
 
   def two_dimensional_code
-    code = rand(10)+rand(100)+rand(1000)
+    code = rand(10)+rand(100)+rand(1000)+rand(10000)
     @code = code.to_s
     @url = 'http://192.168.1.132:3001/sessions/login/'+@code
   end
@@ -16,20 +16,20 @@ class UsersController < ApplicationController
     data = JSON.parse RestClient.get('http://192.168.1.132:3001/request/data/'+params[:user_name])
     @code = data["code"]
     @name = data["user"]
-    @activities = data['activities'].sort_by {|activity| activity['activity_id'].to_i}
+    activities = data['activities'].sort_by {|activity| activity['activity_id'].to_i}
     data['sign_ups'].group_by{|sign_up| sign_up['activity_id']}.each do |key,value|
       sign_ups.push({:activity_id=>key,:num=>value.length})
     end
     data['bid_lists'].group_by {|bid_list| bid_list['activity_id']}.each do |key,value|
       bid_lists.push({:activity_id=>key,:num=>value.length})
     end
-    @activities.each do |t|
+    activities.each do |t|
       t['bid_num'] = get_num bid_lists,t['activity_id']
       t['sign_up_num'] = get_num sign_ups,t['activity_id']
     end
-    session[:aa] = @activities
-    @aa = session[:aa].paginate(page: params[:page], per_page: 10)
-    session[:aa] = nil
+    session[:activities] = activities
+    @activities = session[:activities].paginate(page: params[:page], per_page: 10)
+    session[:activities] = nil
   end
 
   def get_num (array, id)
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
   end
 
   def logout
+    redirect_to root_path
   end
 
   def is_used
--- app/models/activity_inf.rb
+++
@@ -1,9 +0,0 @@
-class ActivityInf < ActiveRecord::Base
-  def self.update_data  activity
-     activity.each do |t|
-       ActivityInf.create(:activity_id=>activity[:activity_id],:bid_num=>activity[:bid_num],
-                          :sign_up_num=>activity[:sign_up_num])
-     end
-  end
-
-end
