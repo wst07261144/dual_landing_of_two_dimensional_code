59545c5f49786d7ceb346d44fdfacb08ca866959
wst07261144 <wst07261144@gmail.com>
2014-01-14 10:38:23 +0800

    修改代码格式

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,22 +7,16 @@ class UsersController < ApplicationController
   def two_dimensional_code
     code = rand(10)+rand(100)+rand(1000)+rand(10000)
     @code = code.to_s
-    @url = 'http://192.168.1.132:3001/sessions/login/'+@code
+    @url = 'http://192.168.1.130:3001/sessions/login/'+@code
   end
 
   def show
-
-    a=[],sign_ups=[],bid_lists=[]
-    data = JSON.parse RestClient.get('http://192.168.1.132:3001/request/data/'+params[:user_name])
+    data = JSON.parse RestClient.get('http://192.168.1.130:3001/request/data/'+params[:user_name])
     @code = data["code"]
     @name = data["user"]
     activities = data['activities'].sort_by {|activity| activity['activity_id'].to_i}
-    data['sign_ups'].group_by{|sign_up| sign_up['activity_id']}.each do |key,value|
-      sign_ups.push({:activity_id=>key,:num=>value.length})
-    end
-    data['bid_lists'].group_by {|bid_list| bid_list['activity_id']}.each do |key,value|
-      bid_lists.push({:activity_id=>key,:num=>value.length})
-    end
+    sign_ups = get_lists  data['sign_ups']
+    bid_lists = get_lists  data['bid_lists']
     activities.each do |t|
       t['bid_num'] = get_num bid_lists,t['activity_id']
       t['sign_up_num'] = get_num sign_ups,t['activity_id']
@@ -32,11 +26,17 @@ class UsersController < ApplicationController
     session[:activities] = nil
   end
 
+  def get_lists  datas
+    list_array=[]
+    datas.group_by{|data| data['activity_id']}.each do |key,value|
+      list_array.push({:activity_id=>key,:num=>value.length})
+    end
+    return list_array
+  end
+
   def get_num (array, id)
     array.each do |t|
-      if t[:activity_id].to_i == id.to_i
-        return t[:num]
-      end
+      return t[:num] if t[:activity_id].to_i == id.to_i
     end
     return 0
   end
@@ -46,11 +46,11 @@ class UsersController < ApplicationController
   end
 
   def is_used
-    render :text =>  RestClient.get('http://192.168.1.132:3001/code/used/'+params[:code])
+    render :text =>  RestClient.get('http://192.168.1.130:3001/code/used/'+params[:code])
   end
 
   def is_logout
-    render :text=> RestClient.get('http://192.168.1.132:3001/code/logout/'+params[:code])
+    render :text=> RestClient.get('http://192.168.1.130:3001/code/logout/'+params[:code])
   end
 
 end
